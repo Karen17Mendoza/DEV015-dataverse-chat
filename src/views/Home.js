3b999cc983b0724236c09262e842cbc17ddc0df5
Franchesca Fatima Zelaya Contreras <fatimapolgar2001@gmail.com>
2024-07-23 10:28:46 -0500

    save

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,14 +2,7 @@ import { navigateTo } from '../router.js';
 import { filterData, sortData, metricsData } from '../lib/dataFunctions.js';
 import { data } from '../data/dataset.js';
 
-export function renderItems(data) {
-  if (!data || !Array.isArray(data)) {
-    return document.createElement("ul");
-  }
-
-  const ulElement = document.createElement("ul");
-
-  data.forEach(item => {
+const renderItem = (item) => {
     const liElement = document.createElement("li");
     liElement.classList.add("cardli");
     liElement.setAttribute("itemscope", "");
@@ -28,22 +21,26 @@ export function renderItems(data) {
         </div>
       </div>
     `;
+    return liElement;  
+  };
+  
+function renderItems(data) {
 
+  if (!data || !Array.isArray(data)) {
+    return document.createElement("ul");
+  }
+
+  const ulElement = document.createElement("ul");
+  data.forEach(item => {
+    const liElement = renderItem(item);
     ulElement.appendChild(liElement);
-  });
-
+  })
   return ulElement;
 }
 
 export const Home = (props) => {
-  const el = document.createElement('div');
-  el.textContent = `¡Bienvenido a la página de inicio, ${props.name || ''}!`;
-
-  // Crear un botón para navegar a la página About
-  const button = document.createElement('button');
-  button.textContent = 'Go to About';
-  button.addEventListener('click', () => navigateTo('/about', { name: 'Xochitl' }));
-
-
-  return el;
+  const mainElement = document.createElement('div');
+  const ulElement = renderItems(data);
+  mainElement.appendChild(ulElement);
+  return mainElement;
 };
