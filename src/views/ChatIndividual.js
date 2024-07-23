a23ec3ed17ff5a777319acf6b995da61a0db9ea2
Franchesca Fatima Zelaya Contreras <fatimapolgar2001@gmail.com>
2024-07-23 16:05:51 -0500

    save

--- src/views/ChatIndividual.js
+++ src/views/ChatIndividual.js
@@ -1,4 +1,4 @@
-export const Details = (props) => {
+export const ChatIndividual = (props) => {
   const el = document.createElement('div');
   el.textContent = 'This is the About page.';
   return el;
