3b096fa6662d018562790f4a70b2462e35edb541
Franchesca Fatima Zelaya Contreras <fatimapolgar2001@gmail.com>
2024-08-05 11:51:32 -0500

    save

--- src/views/ChatIndividual.js
+++ src/views/ChatIndividual.js
@@ -9,11 +9,27 @@ export const ChatIndividual = (props) => {
   const { id } = props;
   const item = getItemById(id);
   const chatElement = document.createElement('div');
+  chatElement.setAttribute("itemscope", "");
+  chatElement.setAttribute("itemtype", "https://schema.org/CreativeWork");
+  chatElement.setAttribute("data-id", item.id);
 
   if (item) {
     chatElement.innerHTML = `
     <h2>Chat con ${item.name}</h2>
-    <!-- Aquí va el contenido del chat -->
+    <div class="chat-header">
+      <img src="${item.imageUrl}" alt="${item.name}" class="profile-pic" itemprop="image">
+      <div class="chat-header-info">
+        <span class="user-name" itemprop="name">${item.name}</span>
+        <span class="user-status">Online</span>
+      </div>
+      </div>
+      <div class="chat-messages"></div>
+      <div class="chat-input">
+        <input type="text" placeholder="Escribe tu mensaje aqui">
+        <button class="send-button">
+          <span class="material-icons">send</span>
+        </button>
+    </div>
   `;
   } else {
     chatElement.textContent = 'Item not found';
