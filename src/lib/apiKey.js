85d8bae5760ed0e1f94cee713856ec62553ddf3c
Karen Mendoza <163893381+Karen17Mendoza@users.noreply.github.com>
2024-08-07 10:41:29 -0500

    saves changes

--- src/lib/apiKey.js
+++
@@ -1,9 +0,0 @@
-export const getApiKey = () => {
-  // Obtener la API KEY desde Local Storage
-  return localStorage.getItem('apiKey');
-};
-  
-export const setApiKey = (key) => {
-  // Guardar la API KEY en Local Storage
-  localStorage.setItem('apiKey', key);
-};
